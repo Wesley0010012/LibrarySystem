3fd39238cc0ffe1ef41da650f4dfae6945017eda
Wesley0010012 <wglaurindo33@gmail.com>
2023-07-10 20:40:25 -0300

    test: Initial test

---
+++ application/backend/Tests/FirstTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class FirstTest extends TestCase {
+  public function testShouldOK(): void {
+    $this->assertEquals(true, true);
+  }
+}
